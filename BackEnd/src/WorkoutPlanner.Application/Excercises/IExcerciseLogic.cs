1c8a1cfcc7bdd99a5ffd83c28ec3a12ca5da30e4
MartinObes <obesmartin03@gmail.com>
2026-03-08 20:29:50 -0300

    create  and delete excercise function and test coverage

---
+++ BackEnd/src/WorkoutPlanner.Application/Excercises/IExcerciseLogic.cs
@@ -0,0 +1,9 @@
+﻿using WorkoutPlanner.Domain;
+
+namespace WorkoutPlanner.Application.Excercises;
+
+public interface IExcerciseLogic
+{
+    public Excercise CreateExcercise(string name);
+    public void DeleteExcercise(Excercise excercise);
+}
---
+++ BackEnd/tests/WorkoutPlanner.Application.Tests/ExcerciseLogicTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using WorkoutPlanner.Application.Excercises;
+
+namespace WorkoutPlanner.Application.Tests;
+
+[TestClass]
+public class ExcerciseLogicTest
+{
+    private ExcerciseLogic _excerciseLogic = null!;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        _excerciseLogic = new ExcerciseLogic();
+    }
+    
+    [TestMethod]
+    public void CreateExcercise_WhenValidInput_ReturnsExcercise()
+    {
+        // Arrange
+        string name = "Push Up";
+        
+        // Act
+        var result = _excerciseLogic.CreateExcercise(name);
+        
+        // Assert
+        result.Should().NotBeNull();
+        result.Name.Should().Be("Push Up");
+        result.Id.Should().NotBeEmpty();
+    }
+    
+    [TestMethod]
+    public void CreateExcercise_WhenNameContainsSpecialCharacters_ThrowsArgumentException()
+    {
+        // Arrange
+        string name = "Push@Up";
+        
+        // Act
+        Action act = () => _excerciseLogic.CreateExcercise(name);
+        
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("Excercise name cannot contain special characters.");
+    }
+    
+    [TestMethod]
+    public void CreateExcercise_WhenNameIsEmpty_ThrowsArgumentException()
+    {
+        // Arrange
+        string name = "   ";
+        
+        // Act
+        Action act = () => _excerciseLogic.CreateExcercise(name);
+        
+        // Assert
+        act.Should().Throw<ArgumentException>().WithMessage("Excercise name cannot be empty.");
+    }
+    
+}
