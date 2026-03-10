df1009aad87f9bd37516c4e891f46f062ee9140d
MartinObes <obesmartin03@gmail.com>
2026-03-10 18:16:34 -0300

    evaluation repo

---
+++ BackEnd/src/WorkoutPlanner.Application/Interfaces/Repositories/IEvaluationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using WorkoutPlanner.Domain;
+
+namespace WorkoutPlanner.Application.Interfaces.Repositories;
+
+public interface IEvaluationRepository : IGenericRepository<Evaluation>
+{
+    Task<Evaluation?> GetEvaluationWithIncludesAsync(Expression<Func<Evaluation, bool>> predicate);
+    Task<IList<Evaluation>> GetAllEvaluationsWithIncludesAsync(Expression<Func<Evaluation, bool>>? predicate = null);
+}
