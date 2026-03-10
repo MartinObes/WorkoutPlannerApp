830950180efc09b965bd6ded4bbf26adeeb1f125
MartinObes <obesmartin03@gmail.com>
2026-03-10 18:32:41 -0300

    workoutexcercise repo

---
+++ BackEnd/src/WorkoutPlanner.Application/Interfaces/Repositories/IWorkoutExcerciseRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using WorkoutPlanner.Domain;
+
+namespace WorkoutPlanner.Application.Interfaces.Repositories;
+
+public interface IWorkoutExcerciseRepository : IGenericRepository<WorkoutExcercise>
+{
+    Task<WorkoutExcercise?> GetWorkoutExcerciseWithIncludesdAsync(Expression<Func<WorkoutExcercise, bool>> predicate);
+    Task<IList<WorkoutExcercise>> GetAllWorkoutExcerciseWithIncludesAsync(Expression<Func<WorkoutExcercise, bool>>? predicate = null);
+    Task<IList<WorkoutExcercise>> GetAllByWorkoutIdAsync(Guid workoutId);
+}
