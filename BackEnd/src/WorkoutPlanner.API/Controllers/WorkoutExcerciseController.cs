6e7b8705bf68a65400842598b5d2d017825c4606
MartinObes <obesmartin03@gmail.com>
2026-03-16 20:58:23 -0300

    workoutexcercise controller

---
+++ BackEnd/src/WorkoutPlanner.API/Controllers/WorkoutExcerciseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WorkoutPlanner.API.Models;
+using WorkoutPlanner.Application.Services.WourkoutProcessorService;
+using WorkoutPlanner.Application.WorkoutExcercises;
+
+namespace WorkoutPlanner.API.Controllers;
+
+[ApiController]
+[Route("workout-excercises")]
+public class WorkoutExcerciseController(IWorkoutExcerciseLogic workoutExcerciseLogic) : ControllerBase
+{
+    private readonly IWorkoutExcerciseLogic _workoutExcerciseLogic = workoutExcerciseLogic;
+    
+    [HttpPut]
+    public async Task<WorkoutExcerciseResponseDto> Update([FromBody] UpdateWorkoutExcerciseRequestDto request)
+    {
+        var result = await _workoutExcerciseLogic.updateWorkoutExcercise(request.WorkoutExcerciseId, request.Name,request.WorkoutId,request.ExcerciseId,
+        request.Reps, request.Sets, request.LoadType, request.Weight, request.Percentage);
+        return new WorkoutExcerciseResponseDto(result);
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteWorkoutExcerciseRequestDto request)
+    {
+        await _workoutExcerciseLogic.deleteWorkoutExcercise(request.WorkoutExcerciseId);
+        return NoContent();
+    }
+    
+    [HttpGet]
+    public async Task<WorkoutExcerciseResponseDto> GetById([FromBody] GetWorkoutExcerciseByIdRequestDto request)
+    {
+        var result = await _workoutExcerciseLogic.getWorkoutExcerciseById(request.WorkoutExcerciseId);
+        return new WorkoutExcerciseResponseDto(result);
+    }
+    
+    [HttpGet]
+    public async Task<WorkoutExcercisesResponseDto> GetAll()
+    {
+        var result = await _workoutExcerciseLogic.getAllWorkoutExcercises();
+        return new WorkoutExcercisesResponseDto(result);
+    }
+
+}
